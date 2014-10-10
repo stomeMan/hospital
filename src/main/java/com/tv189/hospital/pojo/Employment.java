ddd63fce2e765490c8f22cf90d57e782364a6bc5
gaohailiang <gaohailiang@tv189.com>
2014-10-10 17:47:14 +0800

    add

---
+++ src/main/java/com/tv189/hospital/pojo/Employment.java
@@ -0,0 +1,24 @@
+package com.tv189.hospital.pojo;
+
+public class Employment {
+	private String name;
+	private String password;
+	public Employment(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}

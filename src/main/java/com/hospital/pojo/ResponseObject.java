de4bbce2e18e64155fd895646d4936647a260f69
gaohailiang <gaohl0501@gmail.com>
2014-10-28 22:06:55 +0800

    改变字段名，和文档保持一致

--- src/main/java/com/hospital/pojo/ResponseObject.java
+++ src/main/java/com/hospital/pojo/ResponseObject.java
@@ -3,7 +3,7 @@ package com.hospital.pojo;
 public class ResponseObject {
 	private int code;
 	private String message;
-	private Object result;
+	private Object info;
 	public int getCode() {
 		return code;
 	}
@@ -16,18 +16,18 @@ public class ResponseObject {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	public Object getResult() {
-		return result;
-	}
-	public void setResult(Object result) {
-		this.result = result;
-	}
 	
-	public ResponseObject(int code, String message, Object result) {
+	public Object getInfo() {
+		return info;
+	}
+	public void setInfo(Object info) {
+		this.info = info;
+	}
+	public ResponseObject(int code, String message, Object info) {
 		super();
 		this.code = code;
 		this.message = message;
-		this.result = result;
+		this.info = info;
 	}
 	public ResponseObject(){
 		
